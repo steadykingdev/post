933688ad615fd9dc0b81a9756b0ec8b95e650c39
steadykingdev <steadykingdev@gmail.com>
2022-09-26 17:27:36 +0900

    home 수정

--- src/main/java/com/steadykingdev/post/controller/HomeController.java
+++ src/main/java/com/steadykingdev/post/controller/HomeController.java
@@ -1,13 +1,19 @@
 package com.steadykingdev.post.controller;
 
+import com.steadykingdev.post.argumentresolver.Login;
+import com.steadykingdev.post.domain.Member;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-//    @GetMapping("/")
-//    public String home() {
-//        return "";
-//    }
+    @GetMapping("/")
+    public String home(@Login Member loginMember, Model model) {
+
+        model.addAttribute("member", loginMember);
+
+        return "home";
+    }
 }
